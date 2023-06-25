69132f845ee00c861788a337a1bb9cc5a9431e64
MaxwellAt <jeffersonmaxwell1603@gmail.com>
2023-06-25 02:34:25 -0300

    removido cena "historia" e corrigido alguns bugs

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import random
 
 
+
 largura_tela = 1280
 altura_tela = 720
 pygame.init()
@@ -62,36 +63,37 @@ class Personagem(pygame.sprite.Sprite):
         self.rect.center = (100,altura_tela - 100)
 
     def update(self):
+        velocidade_animation = 0.2
         if self.animation == "parado":
             if self.index_lista >= len(self.imagens) - 1:
                 self.index_lista = 0
             else:
-                self.index_lista += 1
-            self.image = self.imagens[self.index_lista]
+                self.index_lista += velocidade_animation
+            self.image = self.imagens[int(self.index_lista)]
         elif self.animation == "direita":
             if self.index_lista >= len(self.ImagemParaDireita) - 1:
                 self.index_lista = 0
             else:
-                self.index_lista += 1
-            self.image = self.ImagemParaDireita[self.index_lista]
+                self.index_lista += velocidade_animation
+            self.image = self.ImagemParaDireita[int(self.index_lista)]
         elif self.animation == "tiro_direita":
             if self.index_lista >= len(self.tiroDireita) - 1:
                 self.index_lista = 0
             else:
-                self.index_lista += 1
-            self.image = self.tiroDireita[self.index_lista]
+                self.index_lista += velocidade_animation
+            self.image = self.tiroDireita[int(self.index_lista)]
         elif self.animation == "tiro_esquerda":
             if self.index_lista >= len(self.tiroEsquerda) - 1:
                 self.index_lista = 0
             else:
-                self.index_lista += 1
-            self.image = self.tiroEsquerda[self.index_lista]
+                self.index_lista += velocidade_animation
+            self.image = self.tiroEsquerda[int(self.index_lista)]
         elif self.animation == "esquerda":
             if self.index_lista >= len(self.ImagemParaEsquerda) - 1:
                 self.index_lista = 0
             else:
-                self.index_lista += 1
-            self.image = self.ImagemParaEsquerda[self.index_lista]
+                self.index_lista += velocidade_animation
+            self.image = self.ImagemParaEsquerda[int(self.index_lista)]
 
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
@@ -150,6 +152,9 @@ class Tiro(pygame.sprite.Sprite):
         self.image = self.imagens[self.index_lista]
         self.rect = self.image.get_rect()
         self.rect.center = (largura_tela - 100, altura_tela - 100)
+        # Achar um bom som para o tiro
+        # pygame.mixer.music.load("Assets\gunTest.mp3")
+        # pygame.mixer.music.play()
 
     def update(self):
         # Ele tem que eliminar a sprit que ele colidiu
@@ -197,11 +202,29 @@ class Inimigo(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         inimigo_imagem = pygame.image.load("Assets/Primeira fase/Pixel_Enemy_Platformer/Orc_Big.png").convert_alpha()
+        tipo = random.choice(["orc", "zombie"])
         self.imagens = []
-        for i in range(3):
-            img = inimigo_imagem.subsurface(pygame.Rect((0, i * 32), (64, 32)))
-            img = pygame.transform.scale(img, (128, 128))
-            self.imagens.append(img)
+        if tipo == "orc":
+            inimigo_imagem = pygame.image.load("Assets/Primeira fase/Pixel_Enemy_Platformer/Orc_Big.png").convert_alpha()
+            for i in range(3):
+                img = inimigo_imagem.subsurface(pygame.Rect((0, i * 32), (64, 32)))
+                img = pygame.transform.scale(img, (128, 128))
+                self.imagens.append(img)
+        elif tipo == "zombie":
+            inimigo_imagem = pygame.image.load("Assets/Primeira fase/Pixel_Enemy_Platformer/Zombie_Big.png").convert_alpha()
+            for i in range(3):
+                img = inimigo_imagem.subsurface(pygame.Rect(( 32, i * 32), (32, 32)))
+                img = pygame.transform.scale(img, (128, 128))
+                self.imagens.append(img)
+        elif tipo == "Skeleton":
+            # Corrigir depois
+            inimigo_imagem = pygame.image.load("Assets/Primeira fase/Pixel_Enemy_Platformer/Skeleton_Big.png").convert_alpha()
+            for i in range(8):
+                img = inimigo_imagem.subsurface(pygame.Rect(( i * 47,  32/2+5), (32, 32)))
+                img = pygame.transform.scale(img, (128, 128))
+                self.imagens.append(img)
+        
+
         self.index_lista = 0
         self.image = self.imagens[self.index_lista]
         self.rect = self.image.get_rect()
@@ -213,8 +236,9 @@ class Inimigo(pygame.sprite.Sprite):
         if self.index_lista >= len(self.imagens) - 1:
             self.index_lista = 0    
         else:
-            self.index_lista += 1
-        self.image = self.imagens[self.index_lista]
+            self.index_lista += 0.25
+
+        self.image = self.imagens[int(self.index_lista)]
         self.rect.x += random.randint(-15, 1)
         self.rect.y += random.randint(-2, 2)
 
@@ -234,7 +258,7 @@ inimigo = Inimigo()
 class PauseButton(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        playbutton = pygame.image.load("Assets/Lucid V1.2/PNG/Flat/64/Play.png")
+        playbutton = pygame.image.load("Assets/Lucid V1.2/PNG/Flat/64/Pause.png")
         playbutton = pygame.transform.scale(playbutton, (32, 32))
         self.image = playbutton
         self.rect = self.image.get_rect()
@@ -253,6 +277,23 @@ class PauseButton(pygame.sprite.Sprite):
 botaoPause = PauseButton()
 
 
+class arma(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("Assets/gun.jpg").convert_alpha().subsurface(pygame.Rect((0, 0), (32, 32))) # achar uma boa imagem para ser a arma
+        self.image = pygame.transform.scale(self.image, (128, 128))
+        self.rect = self.image.get_rect()
+        self.rect.center = (largura_tela - 100, altura_tela - 100)
+    
+    def update(self):
+        self.rect.center = (largura_tela - 100, altura_tela + 100)
+        if pygame.sprite.collide_rect(self, personagem):
+            self.rect.center = personagem.rect.center
+        else:
+            self.rect.center = (largura_tela - 100, altura_tela - 100)
+
+
+
 def PrimeiraFase():
     global pontos
     global cena
@@ -269,6 +310,10 @@ def PrimeiraFase():
 
     allSprites_group.add(lifebar)
 
+    # gun = arma()
+    # allSprites_group.add(gun)
+
+
     if pontos < 1 :
         allSprites_group.add(inimigo)
 
@@ -277,33 +322,36 @@ def PrimeiraFase():
     if pontos >= 20:
         allSprites_group.empty()
         screen.blit(titleFonte("Parabéns, você passou de fase", True, "WHITE"), (largura_tela // 2 - 400, altura_tela // 2 - 100))
-        cena = 3
-        # screen.blit(titleFonte("Infelizmente não ha mais fases", True, "WHITE"), (largura_tela // 2 - 300, altura_tela // 2 - 50))
-        # screen.blit(titleFonte("Aperte na tecla 'Q' para voltar ao menu", True, "WHITE"), (largura_tela // 2 - 250, altura_tela // 2))
+        # cena = 3
+        screen.blit(titleFonte("Infelizmente não ha mais fases", True, "WHITE"), (largura_tela // 2 - 300, altura_tela // 2 - 50))
+        screen.blit(titleFonte("Aperte na tecla 'Q' para voltar ao menu", True, "WHITE"), (largura_tela // 2 - 250, altura_tela // 2))
         # pontos = 0
 
     # colocar a pontuação na tela
 
 
 num_frame = 30
-cena = 1
+cena = 0
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if cena == 1:
+        if cena == 0:
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
                     if pygame.mouse.get_pos()[0] > (largura_tela // 2) - 190 and pygame.mouse.get_pos()[0] < (largura_tela // 2) + 180:
                         if pygame.mouse.get_pos()[1] > (altura_tela // 2) - 90 and pygame.mouse.get_pos()[1] < (altura_tela // 2) + 0:
-                            cena = "historie"
-        # elif cena >= 2:
-        #     if event.type == pygame.MOUSEBUTTONDOWN:
-        #         if event.button == 1:
-        #             if pygame.mouse.get_pos()[0] > ((largura_tela//2)-10) and pygame.mouse.get_pos()[0] < ((largura_tela//2)-10)+32:
-        #                 if pygame.mouse.get_pos()[1] > 25 and pygame.mouse.get_pos()[1] < 25+32:
-        #                     cena = 1
-
+                            cena = 1
+        elif cena>= 2:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    if pygame.mouse.get_pos()[0] > ((largura_tela//2)-10) and pygame.mouse.get_pos()[0] < ((largura_tela//2)-10)+32:
+                        if pygame.mouse.get_pos()[1] > 25 and pygame.mouse.get_pos()[1] < 25+32:
+                            cena = 1
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    cena = 0
+                    pontos = 0
 
 
 
@@ -312,7 +360,7 @@ while running:
     screen.fill("black")
 
 
-    if cena == 1:
+    if cena == 0:
         #posições das coisas
         playPos = ((largura_tela // 2) + 80, (altura_tela // 2)-80)
         playbutton = pygame.image.load("./Assets/Lucid V1.2/PNG/Flat/64/Play.png")
@@ -320,13 +368,16 @@ while running:
         pygame.draw.rect(screen, "WHITE", ((largura_tela // 2) - 190, (altura_tela // 2)-90, 370, 90), 2)
         screen.blit(playbutton, playPos)
         screen.blit(titleFonte("Play Game", True, "WHITE"), ((largura_tela // 2)-170, (altura_tela // 2)-70))
-    elif cena == "historie":
+    elif cena == 1:
         personagem_imagem = pygame.image.load("Assets/Player/Idle.png").convert_alpha()
-        screen.blit(pygame.transform.scale(personagem_imagem.subsurface(pygame.Rect((0 * 32, 0), (32, 32))),(1280, 1280)), (-300, -40))
+        screen.blit(pygame.transform.scale(personagem_imagem.subsurface(pygame.Rect((0 * 32, 0), (32, 32))),(1280, 1280)), (-350, -40))
         screen.blit(titleFonte("Historia", True, "WHITE"), (170, 70))        
         screen.blit(textFont("Olá, meu nome é Steve. Acabo de perder meu gato.", True, "WHITE"), (650, 500))
         screen.blit(textFont("Você poderia me ajudar a encontra-lo?", True, "WHITE"), (650, 550))
         screen.blit(dicaFont("Aperte na tecla 'L' para ajudar o steve e 'Q' para voltar ao menu.", True, "ORANGE"), (900, 100))
+        # desenhar uma caixa de dialogo em volta do texto com poligono
+        pygame.draw.polygon(screen, "WHITE", ((550,600),(640, 490), (1240, 490), (1240, 590), (640, 590)), 2)
+        # pygame.draw.rect(screen, "WHITE", (640, 490, 600, 100), 2)
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_l:
